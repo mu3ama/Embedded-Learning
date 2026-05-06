cdf4af34f6375632de805999a2ab8575619e805b
mu3ama <engr.usamasiddique@gmail.com>
2026-05-06 09:43:52 +0500

    day1 practice

---
+++ week1/day1/hello.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+    
+/*void addTen(int *p) {// passing the pointer to a function adn changeing the orginal data
+    *p = *p + 10;
+}
+
+int main() {
+    int x = 5;
+    addTen(x);
+    printf("x = %d\n", x);
+    return 0;
+}*/
+/*int main() //change the integer value using pointer
+{
+    int age =28;
+   int  *p =&age;
+
+    *p = 30;
+    printf("age %d",age);
+    return 0;
+
+}*/
+/*int main() { / example 1
+    int x = 10;
+    int *p = &x;
+
+    printf("Value of x = %d\n", x);
+    printf("Address of x = %p\n", &x);
+    printf("Value of p (address it holds) = %d\n", *p);
+    printf("Value at the address p points to = %p\n", p);
+
+    return 0;
+}*/
---
+++ week1/day1/readme.md
@@ -0,0 +1,1 @@
+On day 1 i learned how to declared pointers ,how to change original value using pointer , and how to pass the pointers to a function and how function works with some examples
